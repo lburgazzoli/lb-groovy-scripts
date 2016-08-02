b4b14c7b62133b51f8da92319474f03c49d84c81
lburgazzoli <lburgazzoli@gmail.com>
2016-08-02 09:34:12 +0200

    improvements

--- src/KubernetesClientTest.groovy
+++ src/KubernetesClientTest.groovy
@@ -1,16 +1,18 @@
-@Grab(group='org.slf4j', module='slf4j-simple', version='1.7.14')
-@Grab(group='io.fabric8', module='kubernetes-client', version='1.3.100')
+@Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21')
+@Grab(group='io.fabric8', module='kubernetes-client', version='1.3.102')
 
 import org.slf4j.*
+import org.slf4j.impl.*
 import io.fabric8.kubernetes.client.*
 
-def logger = LoggerFactory.getLogger('k8s')
-def config = new ConfigBuilder()
-    .withMasterUrl('https://172.28.128.4:8443')
-    .build()
+System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
 
-new DefaultKubernetesClient(config).withCloseable {
-    it.configMaps().withLabels([ 'karaf-id': 'karaf-1' ]).list().items.each {
-        logger.info("map ${it}")
-    }
+def log = LoggerFactory.getLogger('k8s')
+def url = System.getenv('KUBERNETES_MASTER')    ?: 'https://192.168.99.100:8443'
+def ns  = System.getenv('KUBERNETES_NAMESPACE') ?: 'default'
+def cfg = new ConfigBuilder().withNamespace(ns).withMasterUrl(url).build()
+
+new DefaultKubernetesClient(cfg).withCloseable {
+    def map = it.configMaps().withName('map2').get()
+    log.info("got > ${map?.data}")
 }
