380c8b7e0292e685219b0b3fc7d4e203524b06b3
lburgazzoli <lburgazzoli@gmail.com>
2016-08-19 15:32:32 +0200

    fixes

--- src/KubernetesClientTest.groovy
+++ src/KubernetesClientTest.groovy
@@ -1,11 +1,11 @@
 @Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21')
-@Grab(group='io.fabric8', module='kubernetes-client', version='1.4.0')
+@Grab(group='io.fabric8', module='kubernetes-client', version='1.4.4')
 
 import org.slf4j.*
 import org.slf4j.impl.*
 import io.fabric8.kubernetes.client.*
 
-System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
+System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
 
 def log = LoggerFactory.getLogger('k8s')
 def url = System.getenv('KUBERNETES_MASTER')    ?: 'https://192.168.99.100:8443'
@@ -13,6 +13,13 @@ def ns  = System.getenv('KUBERNETES_NAMESPACE') ?: 'default'
 def cfg = new ConfigBuilder().withNamespace(ns).withMasterUrl(url).build()
 
 new DefaultKubernetesClient(cfg).withCloseable {
-    def map = it.configMaps().withName('map2').get()
-    log.info("got > ${map?.data}")
+    def map = it.configMaps().withName('map-props2').get()
+    map?.data.each {
+        k,v -> log.info("got key=${k}, val=${v}")
+
+        //def p = new Properties()
+        //p.load(new StringReader(v))
+
+        //log.info("got ${p}")
+    }
 }
