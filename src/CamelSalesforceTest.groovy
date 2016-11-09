9f9374a6efc492a7171fd65b926d88c252a9668d
lburgazzoli <lburgazzoli@gmail.com>
2016-11-09 10:33:07 +0100

    fix

--- src/CamelSalesforceTest.groovy
+++ src/CamelSalesforceTest.groovy
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.thoughtworks.xstream.annotations.XStreamAlias
+import com.thoughtworks.xstream.annotations.XStreamImplicit
 @Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21')
 @Grab(group='org.apache.camel', module='camel-core', version='2.18.0')
 @Grab(group='org.apache.camel', module='camel-salesforce', version='2.18.0')
@@ -6,16 +9,10 @@ import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.component.salesforce.SalesforceComponent
 import org.apache.camel.component.salesforce.SalesforceEndpointConfig
 import org.apache.camel.component.salesforce.SalesforceLoginConfig
+import org.apache.camel.component.salesforce.api.dto.AbstractQueryRecordsBase
+import org.apache.camel.component.salesforce.api.dto.AbstractSObjectBase
 import org.apache.camel.component.salesforce.internal.dto.NotifyForFieldsEnum
 import org.apache.camel.main.Main
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.thoughtworks.xstream.annotations.XStreamConverter;
-import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import org.apache.camel.component.salesforce.api.PicklistEnumConverter;
-import org.apache.camel.component.salesforce.api.dto.AbstractSObjectBase;
-import org.apache.camel.component.salesforce.api.dto.AbstractQueryRecordsBase;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 // **************************
 //
 // **************************
@@ -45,7 +42,6 @@ ecfg.notifyForOperationDelete = true
 ecfg.notifyForOperationUndelete = true
 ecfg.notifyForOperationUpdate = true
 ecfg.notifyForFields = NotifyForFieldsEnum.ALL
-ecfg.sObjectClass = "Case"
 
 def lcfg = new SalesforceLoginConfig()
 lcfg.userName = System.getenv('SALESFORCE_USERNAME')
@@ -67,12 +63,14 @@ m.addRouteBuilder(new RouteBuilder() {
     void configure() {
         from('salesforce:camel-test?updateTopic=true&sObjectQuery=SELECT Id FROM Case')
             .setHeader('sObjectQuery')
-                .simple('SELECT Id,CreatedById,CreatedDate,CaseNumber,Subject,Description FROM Case WHERE Id=\'${in.body.id}\'')
+                .simple('SELECT Id,CreatedById,CreatedDate,CaseNumber,Subject,Description FROM Case WHERE Id=\'${in.body[Id]}\'')
             .setHeader('sObjectClass')
                 .simple(CaseRecords.class.name)
-                .to('log:salesforce-event?level=INFO&showHeaders=true&multiline=true')
-            .enrich('salesforce:query')
-                .to('log:salesforce-query?level=INFO&showHeaders=true&multiline=true')
+            .enrich('salesforce:query', { e,d ->
+                    e.in.body = d.in.getBody(CaseRecords.class).records[0];
+                    return e
+                 })
+            .to('log:salesforce-query?level=INFO&showHeaders=true&multiline=true')
     }
 })
 
